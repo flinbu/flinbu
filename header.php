ab6c10238f7bf2f5621dc9af538fe7d233355540
falinareselpais <falinares@elpais.com.co>
2016-03-16 19:21:59 -0500

    Theme configuration

--- header.php
+++ header.php
@@ -5,3 +5,11 @@
   </head>
   <body <?php body_class(); ?>>
     <?php open_body(); ?>
+    <main>
+      <?php
+        if (is_home() || is_front_page()) {
+          get_template_part('templates/header/header', 'home');
+        } else {
+          get_template_part('templates/header/header', 'common');
+        }
+      ?>
--- inc/autoload.php
+++ inc/autoload.php
@@ -1,4 +1,5 @@
 <?php
+  get_template_part('inc/theme/mobile', 'detect'); //Mobile Detect
   get_template_part('inc/theme/functions'); //Functions
   get_template_part('inc/theme/ajax'); //Ajax
   get_template_part('inc/theme/bootstrap', 'menu'); //Bootstrap Walker
@@ -9,6 +10,6 @@
   get_template_part('inc/theme/media'); //Media
   get_template_part('inc/theme/menu'); //Menu
   get_template_part('inc/theme/post', 'types'); //Post Types
-  get_template_part('inc/theme/sidebar'); //Sidebar 
+  get_template_part('inc/theme/sidebar'); //Sidebar
   get_template_part('inc/theme/taxonomy'); //Taxonomy
 ?>
